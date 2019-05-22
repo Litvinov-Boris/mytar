0d0c15fb4b7a663799f6af0f23aa0bd37457dfc7
Litvinov-Boris <boris.litvinov2017@yandex.ru>
2019-05-22 20:29:25 +0300

    try1

--- main.c
+++ main.c
@@ -1,4 +1,36 @@
-int main(int argc, char *argv)
+#include "mytar.h"
+
+
+int main(int argc, char **argv)
 {
-    return (0);
+    mode_t flags = 0775;
+    char *name_ar = "test.tar";
+    char *dir_out = NULL;
+    char *path = NULL;
+    int mode = 0;
+    int opt;
+
+    while ((opt = getopt(argc, argv, "u:o:d") != -1))
+    {
+        switch (opt)
+        {
+            case 'o':
+                name_ar = optarg;
+                dir_out = optarg;
+                break;
+            case 'u':
+                mode = UNARCHIVE;
+                path = optarg;
+                break;
+            case 'd':
+                mode = ARCHIVE;
+                path = optarg;
+                break;
+        }
+    }
+    if (mode == 0 || path == NULL) 
+    {
+        fprintf(stderr, "неверно введенные данные");
+        return (-1);
+    }
 }
--- mytar.h
+++ mytar.h
@@ -1,4 +1,37 @@
 #ifndef MYTAR_H
 # define MYTAR_H
 
+#define UNARCHIVE 1
+#define ARCHIVE 2
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <string.h>
+#include <errno.h>
+#include <utime.h>
+#include <stdio.h>
+
+typedef struct  shead
+{
+    char        name[100];
+    char        mode[8];
+    char        uid[8];
+    char        gid[8];
+    char        size[12];
+    char        mtime[12];
+    char        checksum[8];
+    char        linkflag[1];
+    char        linkname[100];
+    char        pad[255];
+}               thead;
+
+
+int             farchive(const char *aname, const char *path)
+void            funarchive();
 #endif
